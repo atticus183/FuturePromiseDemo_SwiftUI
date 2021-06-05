aba108cb226bf85c6735d174e8f8970bcaa212b1
Josh R <seventytwoapp@gmail.com>
2021-06-04 19:39:40 -0500

    Initial commit.

---
+++ FuturePromiseDemo_SwiftUI/FuturePromiseDemo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuturePromiseDemo_SwiftUIApp.swift
+//  FuturePromiseDemo_SwiftUI
+//
+//  Created by Josh R on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct FuturePromiseDemo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
